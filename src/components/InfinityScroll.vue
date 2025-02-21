df9909f1291698496a46b37803d61e36c1b117bb
RubioCosta <rubioc02@gmail.com>
2025-02-21 01:27:43 +0000

    refatorando regra para exibir imagem

--- src/components/InfinityScroll.vue
+++ src/components/InfinityScroll.vue
@@ -1,32 +1,51 @@
 <template>
-  <section id="infinity-scroll-main" v-on:scroll="addInfinityScroll">
-    <div v-for="(item, index) in list">
-      <Loading :key="index" />
+  <section id="infinity-scroll-main" @scroll="addInfinityScroll">
+    <div class="box-image" v-for="(post, index) in postsCurrent">
+      <Post :pathImage="post.url" />
+      <Post :pathImage="post.url" />
     </div>
   </section>
 </template>
 
-<script>
-  import Loading from './Loading.vue';
-  export default {
-    name: 'InfinityScroll',
-    components: {
-      Loading
-    },
-    data() {
-      return {
-        list: [{}]
-      }
-    },
-    methods: {
-      addInfinityScroll(e) {
-        const heightScrollY = e.target.scrollHeight;
-        const positionTop = e.target.scrollTop;
-        const scrollClientView = e.target.clientHeight;
+<script setup>
+  import { onMounted, ref } from 'vue' 
+  import Post from './Post.vue';
 
-        if ((heightScrollY - (positionTop + scrollClientView)) < 60 ) this.list.push({})
-      }
+  const PACK_SIZE = 20
+  const postsCurrent = ref([])
+  const postsStack = ref([])
+
+  onMounted(async () => {
+    getPackPosts() 
+  })
+
+  async function addInfinityScroll(e) {
+    const heightScrollY = e.target.scrollHeight;
+    const positionTop = e.target.scrollTop;
+    const scrollClientView = e.target.clientHeight;
+
+    if ((heightScrollY - (positionTop + scrollClientView)) < 80 ) {
+      await getPackPosts()
     }
+  }
+
+  async function getPackPosts() {
+    const promises = []
+
+    for (let i = 0; i < PACK_SIZE; i++) {
+      promises.push(getPost())
+    }
+
+    const listImages = await Promise.all(promises)
+
+    postsCurrent.value.push(...listImages)
+  }
+
+  async function getPost() {
+    const response = await fetch('https://picsum.photos/800/500')
+    const { url } = response;
+
+    return { url };
   }
 </script>
 
@@ -37,5 +56,9 @@
     box-shadow: 1px 1px 3px #837e7e;
     margin: 5px;
     overflow-y: scroll;
+  }
+
+  .box-image {
+    display: flex;
   }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="post-main">
+    <img :src="pathImage" />
+  </div>
+</template>
+
+<script setup>
+  const { pathImage } = defineProps(['pathImage'])
+</script>
+
+<style>
+  #post-main {
+    margin: 5px;
+    box-shadow: 1px 1px 5px #413838;
+  }
+  
+  img {
+    width: 100%;
+    min-height: 100%;
+  }
+</style>
