39338d4a7f139d649db5c5384d62a697af185a70
RubioCosta <rubioc02@gmail.com>
2025-02-23 23:53:48 +0000

    fix: add rules for steps drag and drop

--- src/components/DraggableCard.vue
+++ src/components/DraggableCard.vue
@@ -4,6 +4,10 @@
     draggable="true"
     @dragstart="handlerDragStart"
     @dragend="handlerDragEnd"
+    @dragenter="handlerDragEnter"
+    @dragleave="handleDragLeave"
+    @dragover="handlerDragOver"
+    @drop="handlerDrop"
     ref="draggableCard"
   >
     {{ titleCard }}
@@ -11,17 +15,50 @@
 </template>
 
 <script setup>
+  // https://web.dev/articles/drag-and-drop?hl=pt-br
+  // https://developer.mozilla.org/pt-BR/docs/Web/API/HTML_Drag_and_Drop_API
+
   import { ref } from 'vue';
   const { titleCard } = defineProps(['titleCard']);
 
   const draggableCard = ref(null);
 
   function handlerDragStart(e) {
-    console.log("Aqui 1")
+    console.log("Iniciando o processo de Draggable.")
+    draggableCard.value.style.opacity = .5;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', draggableCard.value.innerHTML)
   }
   
   function handlerDragEnd(e) {
-    console.log("Aqui 2")
+    console.log("Finalizando o processo de Draggable.")
+    draggableCard.value.style.opacity = 1;
+  }
+  
+  function handlerDragEnter(e) {
+    console.log("Dentro do elemento de Dropped.")
+  }
+  
+  function handleDragLeave(e) {
+    draggableCard.value.style.backgroundColor = '#FFFFFF'
+    console.log("Saindo do elemento de Dropped.")
+  }
+
+  function handlerDragOver(e) {
+    draggableCard.value.style.backgroundColor = '#000000'
+    console.log("Quando o elemento estiver sobre o elemento de Dropped.")
+  }
+
+  function handlerDrop(e) {
+    console.log("Aqui")
+    e.stopPropagation(); // Retirar o o comportamento padrão do navegador para quedas.
+
+    if (draggableCard.value !== e.target) {
+      draggableCard.value = e.target;
+      e.target = e.dataTransfer.getDate('text/html')
+    }
+
   }
 </script>
 
