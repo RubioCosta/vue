075dbd67c2d3466ca9654e2006067473cc6c9254
RubioCosta <rubioc02@gmail.com>
2025-02-23 15:14:23 +0000

    fix: add inital columns for drag and drop

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -1,7 +1,45 @@
 <template>
-  <h1>DragAndScroll</h1>
+  <section id="drag-drop-main">
+    <div class="box-backlog">
+
+    </div>
+    <div class="box-doing">
+
+    </div>
+    <div class="box-done">
+
+    </div>
+  </section>
 </template>
 
 <script setup>
 </script>
 
+<style scoped>
+  #drag-drop-main {
+    margin: 5px;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2;
+  }
+
+  .box-backlog, .box-doing, .box-done {
+    flex: 1;
+    height: 300px;
+  }
+
+  .box-backlog {
+    background-color: #888888;
+  }
+
+  .box-doing {
+    background-color: #AAAAAA;
+  }
+
+  .box-done {
+    background-color: #DDDDDD;
+  }
+
+</style>
