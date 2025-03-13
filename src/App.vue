f746379cf89b6798b2baa458dfd40b20eb209a11
RubioCosta <rubioc02@gmail.com>
2025-03-13 22:35:23 +0000

    fix: add new component

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,7 @@
 <template>
-  <section id="app-main">
-    <ul>
-      <li @click="handlerShowComponent('InfinityScroll')">INFINITY SCROLL</li>
-      <li @click="handlerShowComponent('DragAndDrop')">DRAG AND DROP</li>
-    </ul>
-    <div v-show="typeSelected==='InfinityScroll'">
-      <InfinityScroll />
-      <br />
-      <InfinityScroll />
-    </div>
-    <div v-show="typeSelected==='DragAndDrop'">
-      <DragAndDrop />
-    </div>
-  </section>
+  <DragAndDropUpdate />
 </template>
 
 <script setup>
-  import { ref } from 'vue'; 
-
-  // Components
-  import InfinityScroll from './components/InfinityScroll/InfinityScroll.vue';
-  import DragAndDrop from './components/DragAndDrop/DragAndDrop.vue';
-
-  const typeSelected = ref('DragAndDrop');
-
-  function handlerShowComponent(type) {
-    typeSelected.value = type
-  }
+  import DragAndDropUpdate from './components/DragAndDropUpdate/DragAndDropUpdate.vue'
 </script>
-
-<style scoped>
-  ul {
-    display: flex;
-    flex-direction: column;
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    background-color: #DDDDDD;
-    height: 30px;
-    max-width: 100%;
-    margin: 4px;
-    font-size: 1em;
-    padding-left: 4px;
-    cursor: pointer;
-  }
-  
-  li:hover {
-    background-color: #BBBBBB;
-  }
-
-</style>
---
+++ src/components/DragAndDropUpdate/DragAndDropUpdate.vue
@@ -0,0 +1,100 @@
+<template>
+  <section id="box-drag-drop-update">
+    <div class="menu">
+      <ul>
+        <li v-for="item in itemsMenu">
+          <input type="text" :value="item.title" @click="() => item.isEdit = true" :readonly="item.isEdit">
+        </li>
+      </ul>
+      <button @click="handlerCreateDirectory">CRIAR DIRETÓRIO NOVO</button>
+    </div>
+    <div class="list-items">
+      LIST ITEMS
+    </div>
+  </section>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const itemsMenu = ref([{id: 1, title: 'teste', isEdit: true}])
+
+  function handlerEditItem(t) {
+    console.log("aqui", t)
+  }
+
+</script>
+
+<style scoped>
+  #box-drag-drop-update {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+
+  .menu, .list-items {
+    padding: 5px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    min-height: 100vh;
+    box-sizing: border-box;
+  }
+
+  .menu {
+    flex: 2;
+    background-color: #1898E0;
+  }
+
+  .menu button {
+    width: 100%;
+    height: 30px;
+    background-color: #DDDFFF;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: 700;
+  }
+
+  .menu button:hover {
+    background-color: #DDDDDF;
+  }
+  
+  .list-items {
+    flex: 7;
+    background-color: yellow;
+  }
+
+  ul {
+    width: 100%;
+  }
+
+  ul li {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    list-style: none;
+    margin-bottom: 5px;
+  }
+
+  ul li:hover {
+
+  }
+
+  ul li input {
+    padding-left: 5px;
+    box-sizing: border-box;
+    height: 30px;
+    width: 100%;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    border-radius: 5px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+  }
+
+</style>
